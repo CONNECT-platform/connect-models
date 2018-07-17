4172f4afa766cb6247e47583e2a6a0f3b474d803
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2018-07-17 17:18:36 +0200

    added a snippet to read panel secret from env

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ platform
     nodes: {
       module: [
         'connect-platform/platform/bind/panel',
-        "connect-platform/platform/bind/utils",
+        'connect-platform/platform/bind/utils',
         'panel-generated',
       ]
     }
-  })
-  .start()
+  });
+
+if (process.env.CONNECT_PANEL_SECRET)
+  platform.configure({
+    panel: {
+      secret: process.env.CONNECT_PANEL_SECRET
+    }
+  });
+
+platform.start()
   .then(server => {
     console.log(`running on http://${server.address().address}` +
                 `:${server.address().port}`);
